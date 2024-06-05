c536721ebcb15cd861209a1dcaef4e9219a25a83
harper71 <www.princeazuka99@gmail.com>
2024-06-05 15:59:51 +0100

    commit task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 /**
  * binary_tree_inorder - it is function that goes through
  *	binary tree using in-order traversal
@@ -9,6 +8,7 @@
  *
  * Return: void
  */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
