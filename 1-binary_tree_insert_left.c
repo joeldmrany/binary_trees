aa1452caefc2199b6289d4de70428406a6dd7f05
harper71 <www.princeazuka99@gmail.com>
2024-06-05 20:59:30 +0100

    changes in task 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,13 +27,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 
-	new_node->left = parent->left;
-
 	new_node->right = NULL;
 
 	if (parent->left != NULL)
 	{
+		new_node->left = parent->left;
+
 		parent->left->parent = new_node;
+	}
+	else
+	{
+		new_node->left = NULL;
 	}
 
 	parent->left = new_node;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -27,13 +27,17 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 
-	new_node->right = parent->right;
-
 	new_node->left = NULL;
 
 	if (parent->right != NULL)
 	{
+		new_node->right = parent->right;
+
 		parent->right->parent = new_node;
+	}
+	else
+	{
+		new_node->right = NULL;
 	}
 
 	parent->right = new_node;
