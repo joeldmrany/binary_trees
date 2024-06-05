6ecd2619706a60cd1a14db439725cc99d0647572
harper71 <www.princeazuka99@gmail.com>
2024-06-05 15:58:52 +0100

    commit 7

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,3 +4,18 @@
  * binary_tree_delete - delete tree
  * @tree: pointer to the root node of the tree
  */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+
+	binary_tree_delete(tree->left);
+
+	binary_tree_delete(tree->right);
+
+	free(tree);
+
+}
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -15,11 +15,14 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
+
 	if (tree->left != NULL)
 	{
 		binary_tree_inorder(tree->left, (func));
 	}
+
 	func(tree->n);
+
 	if (tree->right != NULL)
 	{
 		binary_tree_inorder(tree->right, (func));
