f1feeef5807bb98c7760155fbfdfedceeb6e38b8
joeldmrany <joeldmrany1@gmail.com>
2024-06-05 17:13:02 +0300

    it

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,12 +15,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	
+
 	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->parent = parent;
