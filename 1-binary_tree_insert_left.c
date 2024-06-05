9071bbf1c678551937520908bf309d88a0d14c09
harper71 <www.princeazuka99@gmail.com>
2024-06-05 14:39:54 +0100

    task 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,17 +10,23 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (parent == NULL)
-		return (NULL);
 	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 	if (new_node == NULL || parent == NULL)
+	{
 		return NULL;
-	new_node->n = value;
+	}
+
 	new_node->parent = parent;
-	new_node->right = NULL;
+	new_node->n = value;
 	new_node->left = parent->left;
+	new_node->right = NULL;
+
 	if (parent->left != NULL)
+	{
 		parent->left->parent = new_node;
+	}
+
 	parent->left = new_node;
+
 	return (new_node);
 }
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - insert a  node at right
+ * @parent: root node
+ * @value: data to be inserted
+ * Return: new_node
+*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (new_node == NULL || parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->right = parent->right;
+	new_node->left = NULL;
+	if (parent->right != NULL)
+	{
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+
+	return (new_node);
+}
