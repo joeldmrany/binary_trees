c54b4a0b48ecff523880f5ae156fd4e91208e0c0
joeldmrany <joeldmrany1@gmail.com>
2024-06-05 18:23:34 +0300

    task:8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_postorder - function that goes through
- *	a binary tree using post-order traversal
+ *		a binary tree using post-order traversal
  *
  * @tree: pointer to root
  * @func: pionter to function
