6ec947c227e2f0a5ea799c49fc3b50cab6136265
harper71 <www.princeazuka99@gmail.com>
2024-06-05 15:11:40 +0100

    git commit task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,11 +9,17 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
-	
-	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL || parent == NULL)
+	if (parent == NULL)
 	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
 	}
 
