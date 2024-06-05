7174551e51f807a9153811c60f149c72153c3e98
harper71 <www.princeazuka99@gmail.com>
2024-06-05 21:25:22 +0100

    comm

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,15 +7,14 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent != NULL)
-	{
-		return (0);
-	}
-
 	if (node == NULL)
 	{
 		return (0);
 	}
 
+	if (node->parent != NULL)
+	{
+		return (0);
+	}
 	return (1);
 }
