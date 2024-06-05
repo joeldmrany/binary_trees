a426e8b9a3acb98182868a64d3ad4fe68ccd2441
harper71 <www.princeazuka99@gmail.com>
2024-06-05 16:13:36 +0100

    commit changes in header file and task 7

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_delete - delete tree
  * @tree: pointer to the root node of the tree
@@ -17,5 +16,4 @@ void binary_tree_delete(binary_tree_t *tree)
 	binary_tree_delete(tree->right);
 
 	free(tree);
-
 }
