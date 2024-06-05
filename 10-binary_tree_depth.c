d7fef2153328c5a5ec4ca17f4dc1a38aff9a6d38
joeldmrany <joeldmrany1@gmail.com>
2024-06-05 19:48:14 +0300

    task: 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measure depth of the node
+ *
+ * @tree: pointer to root
+ *
+ * Return: depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	int d;
+
+	d = 0;
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	while (tree->parent != NULL)
+	{
+		d++;
+		tree = tree->parent;
+	}
+	return (d);
+}
