ad6f9024e588a208760cc6b7b5d601175d383d93
harper71 <princeazuka99@gmail.com>
2024-06-06 21:01:22 +0100

    commit changes

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -31,6 +31,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		return (1);
 	}
-	return (binary_tree_is_perfect(tree->left) &&
-	binary_tree_is_perfect(tree->right));
+	else
+	{
+		return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+	}
+
+	return (0);
 }
